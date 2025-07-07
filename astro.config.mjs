fb22bb74790dc4d89b18ec24347db8913760766a
Carlos Fraile <93010293+ThePandogs@users.noreply.github.com>
2025-07-07 22:26:13 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
-import staticAdapter from '@astrojs/adapter-static'
 
 export default defineConfig({
-  site: 'https://ThePandogs.github.io/ThePandogs.dev/', // URL completa con repo
-  base: '/ThePandogs.dev/',                             // Nombre del repo con barra
+  site: 'https://ThePandogs.github.io/ThePandogs.dev/', // URL completa
+  base: '/ThePandogs.dev/',                             // nombre del repo con /
   integrations: [tailwind(), robotsTxt()],
-  adapter: staticAdapter(),
 })
