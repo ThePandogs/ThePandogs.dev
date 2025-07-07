9e39d76b85ff70a5616809ff542b4e35faab8aa8
Carlos Fraile <93010293+ThePandogs@users.noreply.github.com>
2025-07-07 22:51:03 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
 
 export default defineConfig({
-  site: 'https://ThePandogs.github.io/ThePandogs.dev/', // URL completa
-  base: '/ThePandogs.dev/',                             // nombre del repo con /
+  site: 'https://thepandogs.dev', // Tu dominio propio sin slash final
+  base: '/',                     // Base raíz porque tu sitio está en la raíz del dominio
   integrations: [tailwind(), robotsTxt()],
 })
-
